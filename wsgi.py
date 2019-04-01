aeeb842a6692d9bc0755dfa8b672a2c100d8da72
Götz Rieger <grieger@redhat.com>
2019-04-01 14:34:15 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -11,17 +11,17 @@ def index():
     return render_template('index.html')
 
 @application.route('/run')
-    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'], headers=headers)
+    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'], headers=headers,verify=False)
     # Example GET invokation of the Robot API       
-    response = requests.get(application.config['URI'] + '/distance' + '?user_key=' + application.config['APITOKEN'], headers=headers)  
+    response = requests.get(application.config['URI'] + '/distance' + '?user_key=' + application.config['APITOKEN'], headers=headers, verify=False)  
         
      # Example POST invokation of the Robot API       
-     #response = requests.post(application.config['URI'] + '/forward/5' + '?user_key=' + application.config['APITOKEN'], headers=headers)
+     #response = requests.post(application.config['URI'] + '/forward/5' + '?user_key=' + application.config['APITOKEN'], headers=headers, verify=False)
     return render_template('result.html', message=str(response.text))
     
 @application.route('/status')
 def status():
-    response = requests.get(application.config['URI'] + '/status' + '?user_key=' + application.config['APITOKEN'], headers=headers)
+    response = requests.get(application.config['URI'] + '/status' + '?user_key=' + application.config['APITOKEN'], headers=headers, verify=False)
     return render_template('result.html', message=str(response.text))
 
 if __name__ == '__main__':
