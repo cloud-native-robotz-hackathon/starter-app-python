e3dbb60138e9ee16ee9b9292c1d067aab5a0f155
Götz Rieger <grieger@redhat.com>
2019-04-03 15:26:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+timeout = 240
 
 URI = 'http://hub-controller-live-hub-controller.apps-9d00.generic.opentlc.com/api/robot'
 APITOKEN = ''
