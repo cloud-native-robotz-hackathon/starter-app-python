a48a3cd4371bcd5e0698ecb1e00f9b8e9dd07683
Götz Rieger <grieger@redhat.com>
2019-06-19 16:59:01 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,27 +4,25 @@ import time
 
 application = Flask(__name__)
 application.config.from_object('config')
-headers = {'accept': 'text/html'}
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/run')
+@application.route('/run', methods=['POST'])
 def run():
-    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'], headers=headers,verify=False)
+    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'],verify=False)
     # Example GET invokation of the Robot API       
-    response = requests.get(application.config['URI'] + '/distance' + '?user_key=' + application.config['APITOKEN'], headers=headers, verify=False)  
+    #response = requests.get(application.config['URI'] + '/distance' + '?user_key=' + application.config['APITOKEN'], verify=False)  
         
-    # Example POST invokation of the Robot API       
-    #response = requests.post(application.config['URI'] + '/forward/5' + '?user_key=' + application.config['APITOKEN'], headers=headers, verify=False)
-    #params = {'user_key': application.config['APITOKEN']} 
-    #response = requests.post(application.config['URI'] + '/forward/5', params=params, headers=headers, verify=False)    
+     # Example POST invokation of the Robot API  
+    data = {'user_key': application.config['APITOKEN']} 
+    response = requests.post(application.config['URI'] + '/forward/5', data=data, verify=False)
     return render_template('result.html', message=str(response.text))
     
-@application.route('/status')
+@application.route('/status', methods=['POST'])
 def status():
-    response = requests.get(application.config['URI'] + '/remote_status' + '?user_key=' + application.config['APITOKEN'], headers=headers, verify=False)
+    response = requests.get(application.config['URI'] + '/remote_status' + '?user_key=' + application.config['APITOKEN'], verify=False)
     return render_template('result.html', message=str(response.text))
 
 if __name__ == '__main__':
