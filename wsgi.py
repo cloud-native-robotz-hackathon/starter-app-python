fbf5ebb23f75ade43c7edb23d665da09ed4b5f2f
Götz Rieger <grieger@redhat.com>
2019-06-20 14:05:20 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -11,13 +11,13 @@ def index():
 
 @application.route('/run', methods=['POST'])
 def run():
-    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'],verify=False)
+    response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'],verify=False)
     # Example GET invokation of the Robot API       
     #response = requests.get(application.config['URI'] + '/distance' + '?user_key=' + application.config['APITOKEN'], verify=False)  
         
-     # Example POST invokation of the Robot API  
-    data = {'user_key': application.config['APITOKEN']} 
-    response = requests.post(application.config['URI'] + '/forward/5', data=data, verify=False)
+    # Example POST invokation of the Robot API  
+    # data = {'user_key': application.config['APITOKEN']} 
+    # response = requests.post(application.config['URI'] + '/forward/5', data=data, verify=False)
     return render_template('result.html', message=str(response.text))
     
 @application.route('/status', methods=['POST'])
