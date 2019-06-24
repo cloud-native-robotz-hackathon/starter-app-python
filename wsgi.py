2418104c4553473deccd3294e1eb3fe18ddbedee
Götz Rieger <grieger@redhat.com>
2019-06-24 15:14:45 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -17,14 +17,15 @@ def run():
     
     # Example POST invocation of the Robot API for e.g. moving  
     data = {'user_key': application.config['APITOKEN']} 
-    response = requests.post(application.config['URI'] + '/forward/5', data=data, verify=False)
-    
-    return render_template('result.html', message=str(response.text))
+    response = requests.post(application.config['URI'] + '/forward/10', data=data, verify=False)
+    return response.text
+    #return render_template('result.html', message=str(response.text))
     
 @application.route('/status', methods=['POST'])
 def status():
     response = requests.get(application.config['URI'] + '/remote_status' + '?user_key=' + application.config['APITOKEN'], verify=False)
-    return render_template('result.html', message=str(response.text))
+    return response.text
+    #return render_template('result.html', message=str(response.text))
 
 if __name__ == '__main__':
    application.run()
