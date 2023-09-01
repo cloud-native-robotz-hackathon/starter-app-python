5fc903fe4e0d0221477746af305833cf41fe1005
Daniel Brintzinger <daniel@nexus-six.de>
2023-09-01 12:31:10 +0200

    updated devfile and readme changed some filenames

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
