f3f8ac333f14b5e60691b5908120d762a18b8ff6
Daniel Brintzinger <daniel@nexus-six.de>
2023-10-12 17:09:17 +0200

    Added object detection inferencing call example

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 import requests
 import time
+from lib.preprocessing import preprocess_encoded_image
+from lib.object_detection import detect_objects
 
 application = Flask(__name__)
 application.config.from_object('config')
+
+class_labels = ['Laptop', 'Computer keyboard', 'Fedora']
 
 @application.route('/')
 def index():
@@ -12,20 +16,36 @@ def index():
 @application.route('/run', methods=['POST'])
 def run():
     # Example GET invocations of the Robot API       
-    response = requests.get('http://' + application.config['URI'] + ':5000/distance', verify=False)  
-    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['APITOKEN'],verify=False)
+    # response = requests.get('http://' + application.config['ROBOT_API'] + ':5000/distance' + '?user_key=' + application.config['API_TOKEN'], verify=False)  
+    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['API_TOKEN'],verify=False)
     
     # Example POST invocation of the Robot API for e.g. moving  
-    response = requests.post('http://' + application.config['URI'] + ':5000/forward/10', verify=False)
-    return response.text
-    #return render_template('result.html', message=str(response.text))	
+    # response = requests.post('http://' + application.config['ROBOT_API'] + ':5000/forward/10', verify=False)
+      
+    # Example calling the ML inferencing endpoint for object detection
+    # get current camera image from robot
+    #img_response = requests.get(application.config['ROBOT_API'] + '/camera', verify=False)
+    # normalize image
+    #image_data, ratio, dwdh = preprocess_encoded_image(img_response.text)
+    # send image to object detection endpoint
+    #objects = detect_objects(
+    #    image_data,
+    #    application.config['INFERENCING_API'] ,
+    #    token='',
+    #    classes_count=len(class_labels),
+    #    confidence_threshold=0.15,
+    #    iou_threshold=0.2
+    )
+    # objects will then contain a list if tensors with box coordinated, confidence and class of detected object
+    #print('Detected {} obejects', len(objects))
 
+    return ('OK')  
+       	
    
 @application.route('/status', methods=['POST'])
 def status():
-    response = requests.get(application.config['URI'] + '/remote_status' , verify=False)
+    response = requests.get(application.config['URI'] + '/remote_status?user_key=' + application.config['API_TOKEN'], verify=False)
     return response.text
-    #return render_template('result.html', message=str(response.text))
 
 if __name__ == '__main__':
    application.run(host="0.0.0.0")
