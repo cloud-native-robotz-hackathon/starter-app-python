205d307d48739a33ead6b88e0128ffadcc41b32b
Daniel Brintzinger <33524976+nexus-Six@users.noreply.github.com>
2024-08-13 18:12:44 +0200

    Added TOKEN

--- app.py
+++ app.py
@@ -15,28 +15,29 @@ def index():
 
 @application.route('/run', methods=['POST'])
 def run():
-    # Example GET invocations of the Robot API       
-    # response = requests.get('http://' + application.config['ROBOT_API'] + ':5000/distance' + '?user_key=' + application.config['API_TOKEN'], verify=False)  
-    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['API_TOKEN'],verify=False)
+    ## Example GET invocations of the Robot API       
+    # response = requests.get('http://' + application.config['ROBOT_API'] + ':5000/distance' + '?user_key=' + application.config['ROBOT_NAME'], verify=False)  
+    # response = requests.get(application.config['URI'] + '/power' + '?user_key=' + application.config['ROBOT_NAME'],verify=False)
     
-    # Example POST invocation of the Robot API for e.g. moving  
+    ## Example POST invocation of the Robot API for e.g. moving  
     # response = requests.post('http://' + application.config['ROBOT_API'] + ':5000/forward/10', verify=False)
       
-    # Example calling the ML inferencing endpoint for object detection
-    # get current camera image from robot
+    ## Example calling the ML inferencing endpoint for object detection
+    ## get current camera image from robot
     #img_response = requests.get(application.config['ROBOT_API'] + '/camera', verify=False)
-    # normalize image
+    ## normalize image
     #image_data, ratio, dwdh = preprocess_encoded_image(img_response.text)
-    # send image to object detection endpoint
+    ## send image to object detection endpoint
+    
     #objects = detect_objects(
     #    image_data,
     #    application.config['INFERENCING_API'] ,
-    #    token='',
-    #    classes_count=len(class_labels),
+    #    token=application.config['INFERENCING_API_TOKEN'],
+    #    classes_count=len(class_labels),        
     #    confidence_threshold=0.15,
     #    iou_threshold=0.2
     #)
-    # objects will then contain a list if tensors with box coordinated, confidence and class of detected object
+    ## objects will then contain a list if tensors with box coordinated, confidence and class of detected object
     #print('Detected {} obejects', len(objects))
 
     return ('OK')  
@@ -44,7 +45,7 @@ def run():
    
 @application.route('/status', methods=['POST'])
 def status():
-    response = requests.get(application.config['URI'] + '/remote_status?user_key=' + application.config['API_TOKEN'], verify=False)
+    response = requests.get(application.config['URI'] + '/remote_status?user_key=' + application.config['ROBOT_NAME'], verify=False)
     return response.text
 
 if __name__ == '__main__':
