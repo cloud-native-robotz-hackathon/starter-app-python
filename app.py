1e006f72bf5d73dadb3f4eaabf44645ba5841a8e
Daniel Brintzinger <daniel@nexus-six.de>
2024-10-24 19:00:35 +0200

    updates, comments und fixes to app

--- app.py
+++ app.py
@@ -3,67 +3,116 @@ import requests
 import time
 from lib.preprocessing import preprocess_encoded_image
 from lib.object_detection import detect_objects
+import threading
+from collections import namedtuple
 
 application = Flask(__name__)
 application.config.from_object('config')
+thread_event = threading.Event()
 
-class_labels = ['Fedora', 'Ball', 'Tennis ball']
+class_labels = ['Fedora',]
+Coordinates = namedtuple('Coordinates', 'confidence_score x_upper_left y_upper_left x_lower_right y_lower_right object_class')
+
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
+
 @application.route('/run', methods=['POST'])
 def run():
-    ## Example GET invocations of the Robot API
+    try:
+        thread_event.set()
 
-    ## Connect and get the status of the robot
-    # response = requests.get(application.config['ROBOT_API'] + '/status' + '?user_key=' + application.config['ROBOT_NAME'], verify=False)
+        thread = threading.Thread(target=startRobot)
+        thread.start()
 
-    ## Get the current power of the robot
-    # response = requests.get(application.config['ROBOT_API'] + '/power' + '?user_key=' + application.config['ROBOT_NAME'],verify=False)
+        return "Robot started"
+    except Exception as error:
+        return str(error)
 
-    ## Example POST invocation of the Robot API for e.g. moving
-    #response = requests.post(application.config['ROBOT_API'] + '/forward/10',data={"user_key": application.config['ROBOT_NAME']} ,verify=False)
 
-    ## Example calling the ML inferencing endpoint for object detection
-    ## get current camera image from robot
-    #img_response = requests.get(application.config['ROBOT_API'] + '/camera'+ '?user_key=' + application.config['ROBOT_NAME'], verify=False)
+@application.route('/stop', methods=['POST'])
+def stop():
+    try:
+        thread_event.clear()
 
-    ## normalize image
-    #image_data, ratio, dwdh = preprocess_encoded_image(img_response.text)
-
-    ## send image to object detection endpoint
-    #objects = detect_objects(
-    #    image_data,
-    #    application.config['INFERENCING_API'] ,
-    #    token=application.config['INFERENCING_API_TOKEN'],
-    #    classes_count=len(class_labels),
-    #    confidence_threshold=0.15,
-    #    iou_threshold=0.2
-    #)
-
-    ## objects will then contain a list if tensors with box coordinates, confidence score and class ( 0 = Fedora) of detected object (x left upper corner, y left upper corner,
-    ## x right lower corner, confidence score, class)
-    #print('objects found ->', objects)
-
-    ## Get the detected fedora object with the highest confidence score
-    #fedora = [0,0,0,0,0,0]
-
-    #for o in objects:
-    #    if o[-1] == 0 and fedora[-2] < o[-2]:
-    #        fedora = o
-
-    #if (fedora[-2] > 0):
-    #    print('Object with highest score -> [ confidence score : {},  x left upper corner : {}, y left upper corner : {}, x right lower corner{}, y right lower corner{}'.format(fedora[-2], fedora[0], fedora[1],fedora[2],fedora[3],))
-
-    return ('OK')
+        return "Robot stopped"
+    except Exception as error:
+        return str(error)
 
 
 @application.route('/status', methods=['POST'])
 def status():
     response = requests.get(application.config['ROBOT_API'] + '/remote_status?user_key=' + application.config['ROBOT_NAME'], verify=False)
     return response.text
+
+
+def startRobot():
+
+     # Drop your code here
+    print('Done')
+
+def take_picture_and_detect_objects():
+    ## Example calling the ML inferencing endpoint for object detection
+    ## get current camera image from robot
+    img_response = requests.get(application.config['ROBOT_API'] + '/camera'+ '?user_key=' + application.config['ROBOT_NAME'], verify=False)
+
+    ## normalize image
+    image_data, ratio, dwdh = preprocess_encoded_image(img_response.text)
+
+    ## send image to object detection endpoint
+    objects = detect_objects(
+        image_data,
+        application.config['INFERENCING_API'] ,
+        token=application.config['INFERENCING_API_TOKEN'],
+        classes_count=len(class_labels),
+        confidence_threshold=0.15,
+        iou_threshold=0.2
+    )
+
+    return objects
+
+
+def find_highest_score(objects):
+    # Initialize a variable to store the coordinates with the highest score
+    detected_object = [0,0,0,0,0,0]
+
+    # Iterate over the detected objects and update the highest score if necessary
+    for o in objects:
+        # Check if the current object is of the class 'Fedora' and has a higher score than the previous highest score
+        if o[-1] == 0 and detected_object[-2] < o[-2]:
+            # Update the detected object with the new highest score
+            detected_object = o
+
+    # Check if there was at least one object detected with a confidence score greater than 0
+    if (detected_object[-2] > 0):
+        # Create a Coordinates namedtuple with the information of the object with the highest score
+        coordinates = Coordinates(detected_object[-2], detected_object[0], detected_object[1], detected_object[2], detected_object[3], detected_object[-1])
+        return coordinates
+
+    return
+
+
+def move_forward(length):
+    response = requests.post(application.config['ROBOT_API'] + '/forward/' + str(length),data={"user_key": application.config['ROBOT_NAME']} ,verify=False)
+    return response.text
+
+
+def move_backward(length):
+    response = requests.post(application.config['ROBOT_API'] + '/backward/' + str(length),data={"user_key": application.config['ROBOT_NAME']} ,verify=False)
+    return response.text
+
+
+def turn_left(degrees):
+    response = requests.post(application.config['ROBOT_API'] + '/left/' + str(degrees),data={"user_key": application.config['ROBOT_NAME']} ,verify=False)
+    return response.text
+
+
+def turn_right(degrees):
+    response = requests.post(application.config['ROBOT_API'] + '/right/' + str(degrees),data={"user_key": application.config['ROBOT_NAME']} ,verify=False)
+    return response.text
+
 
 if __name__ == '__main__':
    application.run(host="0.0.0.0", port=8080)
