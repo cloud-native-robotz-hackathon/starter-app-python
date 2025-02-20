38be0ca96d9a536d9322fa6fd4f1c2c8381578dd
Manuel Hahn <manuel.hahn@gmx.de>
2025-02-20 13:43:25 +0100

    Added changes from Manual

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
 import time
-from lib.preprocessing import preprocess_encoded_image
+from lib.preprocessing import preprocess_encoded_image, preprocess_image_file
 from lib.object_detection import detect_objects
 import threading
 from collections import namedtuple
@@ -50,9 +50,127 @@ def status():
 
 
 def startRobot():
-
-     # Drop your code here
+    # Drop your code here
+    dist = distance()
+    print ('Got distance -> ', dist)
     print('Done')
+
+    # Initialize a variable to store the coordinates of the identified
+    hat_coordinates = None
+
+    hat_aligned = False
+
+    turn_counter = 0
+    while thread_event.is_set():
+        dist = distance()
+        print ('Got distance -> ', dist)
+
+        objects = take_picture_and_detect_objects()
+        coordinates = find_highest_score(objects)
+
+        if coordinates and coordinates.confidence_score > 0.5:
+            print(f'''Object with highest score -> [
+                confidence score: {coordinates.confidence_score},
+                x upper left corner: {coordinates.x_upper_left},
+                y upper left corner: {coordinates.y_upper_left},
+                x lower right corner: {coordinates.x_lower_right},
+                y lower right corner: {coordinates.y_lower_right},
+                object class: {coordinates.object_class} ]''')
+
+            # Determine size of the object in the image (not the real size!)
+            delta = coordinates.x_lower_right - coordinates.x_upper_left
+            print(f'delta: {delta}')
+
+            # Check if one hat could be identified with sufficient probabilty
+            hat_detection_threshold = 0.50 # i.e. 850%
+            if coordinates.confidence_score > hat_detection_threshold:
+                hat_coordinates = coordinates
+                print('Hat identified!')
+
+            if not hat_coordinates:
+                turn_left(10)
+                turn_counter = turn_counter + 10
+
+            # If the hat was identified, align so that the most likely hat is in the center
+            if hat_coordinates:
+                center_x = (coordinates.x_upper_left + coordinates.x_lower_right) / 2
+                print(f'center_x: {center_x}')
+                # if center_x < 320: # TODO switch from pixels to variable that is fed from image information
+                #     turn_left(int(abs(320-center_x)))
+                # else:
+                #     turn_right(int(abs(320-center_x)))
+
+                if abs(640/2-center_x) < 20: # TODO make this a var
+                    hat_aligned = True
+                else:
+                    if center_x < 320: # TODO switch from pixels to variable that is fed from image information
+                        turn_left(10)
+                    else:
+                        turn_right(10)
+
+
+            # Move forward to the hat if it was identified
+            if hat_coordinates and hat_aligned:
+                if (delta > 350 or int(dist) < 300):
+                    print('Done - arrived at the object')
+                    return
+                else:
+                    move_forward(10)
+
+
+
+
+
+            # # Center object that is most likely a hat
+            # center_x = (coordinates.x_upper_left + coordinates.x_lower_right) / 2
+            # if abs(640/2-center_x) < 20: # TODO make this a var
+            #     hat_aligned = True
+            # else:
+            #     if move_x < 320: # TODO switch from pixels to variable that is fed from image information
+            #         turn_left(10)
+            #     else:
+            #         turn_right(10)
+
+
+
+            # # Move forward to the hat if it was identified
+            # if hat_coordinates:
+            #     # Determine size of the object in the image (not the real size!)
+            #     delta = coordinates.x_lower_right - coordinates.x_upper_left
+            #     print(f'delta: {delta}')
+
+
+
+            # if delta < 350:
+            #     move_forward(10)
+            # else:
+            #     print('Done - arrived at the object')
+            #     return
+
+            # # Define stop criterium
+            # if coordinates.confidence_score > 0.8 and (delta > 350 or int(dist) < 300):
+            #     print('Done - arrived at the object')
+            #     return
+            # else:
+            #     move_forward(5)
+
+
+            # if int(dist) > 300: # TODO Add variable for stop distance (here 100 mm)
+            #     move_forward(10)
+            # else:
+            #     print('Done - arrived at the object')
+            #     return
+
+        else:
+            if turn_counter < 360:
+                turn_left(10)
+                turn_counter = turn_counter + 10
+            else:
+                print('Done - no object found')
+                return
+
+    print('Done')
+
 
 def take_picture_and_detect_objects():
     ## Example calling the ML inferencing endpoint for object detection
@@ -77,6 +195,13 @@ def take_picture_and_detect_objects():
         confidence_threshold=0.15,
         iou_threshold=0.2
     )
+
+    ## Add boxes to image
+    from lib.object_rendering import draw_boxes
+    image_path = "static/current_view.jpg"
+    xxx, scaling, padding = preprocess_image_file(image_path)
+    draw_boxes(image_path, objects, scaling, padding, class_labels)
+
 
     return objects
 
@@ -126,4 +251,10 @@ def distance():
 
 
 if __name__ == '__main__':
-   application.run(host="0.0.0.0", port=8080)
+    # Set environment variables
+    import os
+    os.environ["PYTHONDONTWRITEBYTEC"] = "1"
+    os.environ["PATH"] = os.environ["PATH"] + ":/home/user/.local/bin"
+
+    # Run flask service
+    application.run(host="0.0.0.0", port=8080, debug = True)
---
+++ lib/object_rendering.py
@@ -0,0 +1,41 @@
+import random
+import cv2
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def draw_boxes(image_path, model_output, scaling, padding, class_labels):
+    image = cv2.imread(image_path)  # Read image
+    colors = {
+        name: [
+            random.randint(0, 255) for _ in range(3)
+        ] for i, name in enumerate(class_labels)
+    }
+    for i, (x0, y0, x1, y1, score, cls_id) in enumerate(model_output):
+        box = np.array([x0, y0, x1, y1])
+        box -= np.array(padding*2)
+        box /= scaling
+        box = box.round().astype(np.int32).tolist()
+        cls_id = int(cls_id)
+        score = round(float(score), 3)
+        name = class_labels[cls_id]
+        color = colors[name]
+        name += ' '+str(score)
+        cv2.rectangle(image, box[:2], box[2:], color,2)
+        cv2.putText(
+            image,
+            name,
+            (box[0], box[1] - 2),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.75,
+            [0, 255, 0],
+            thickness=2
+        )
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    fig = plt.gcf()
+    fig.set_size_inches(24, 12)
+    plt.axis('off')
+    plt.imshow(img)
+
+    cv2.imwrite(image_path, img)  # write image
